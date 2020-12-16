b747be08e1788311e123d33c91aca8246c94cf44
ahmet emre <ahmet@meditopia.com>
2020-12-16 11:58:54 +0300

    list component done

---
+++ components/List.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="list">
+    <h3>{{ headline }}</h3>
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    headline: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.list {
+  width: 100%;
+
+  h3 {
+    margin-bottom: 20px;
+  }
+}
+</style>
