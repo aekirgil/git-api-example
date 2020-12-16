77c21199077c1c66779f0e8b06f341820535eb88
ahmet emre <ahmet@meditopia.com>
2020-12-16 12:01:16 +0300

    main page done with ui

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,44 @@
 <template>
   <main class="welcome">
-    <div> {{ data }}</div>
-    <nuxt-link :to="`repos/${ data[0].fullname }`">
-      detail
-    </nuxt-link>
+    <div class="container">
+      <section>
+        <Hero :hero-source="hero" />
+      </section>
+      <section v-if="data.length">
+        <List :headline="headline">
+          <div class="row">
+            <div
+              v-for="repo in data"
+              :key="repo.id"
+              class="col-12 col-sm-12 col-md-4"
+            >
+              <Box :title="repo.name">
+                <nuxt-link :to="`repos/${ repo.fullname }`">
+                  <button>
+                    See Repository
+                  </button>
+                </nuxt-link>
+              </Box>
+            </div>
+          </div>
+        </List>
+      </section>
+      <section v-else>
+        <Empty text="Cannot find any repository by the given information" />
+      </section>
+    </div>
   </main>
 </template>
 
 <script>
+import Hero from '@/components/Hero'
+import Box from '@/components/Box'
+import List from '@/components/List'
 
 import { fnSort, ASC } from '@/utilities'
 
 export default {
+  components: { Hero, Box, List },
   async asyncData ({ params, $axios }) {
     const limit = 3; const sortkey = 'stargazers_count'; const url = '/users/homeday-de/repos'
     let data = await $axios.$get(url)
@@ -20,6 +47,13 @@ export default {
   },
   data () {
     return {
+      headline: 'Repository List',
+      hero: {
+        image: '/headerimg.png',
+        headline: 'Welcome',
+        buttonText: 'Go to Workflow ',
+        buttonUrl: '/workflow'
+      }
     }
   }
 }
