8760ded5e3db82f2ef4f29079a7e2b5ee4e100bc
ahmet emre <ahmet@meditopia.com>
2020-12-16 11:56:12 +0300

    hero component done

---
+++ components/Hero.vue
@@ -0,0 +1,56 @@
+<template>
+  <div
+    class="hero"
+    role="img"
+    aria-label="Image Description"
+    :style="`background-image: url(${heroSource.image})`"
+  >
+    <h1>
+      {{ heroSource.headline }}
+    </h1>
+    <nuxt-link v-if="heroSource.buttonText" id="heroBtn" exact :to="heroSource.buttonUrl">
+      <button>{{ heroSource.buttonText }}</button>
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    heroSource: {
+      type: Object,
+      default: () => ({
+        image: {
+          type: String,
+          default: ''
+        },
+        headline: {
+          type: String,
+          default: ''
+        },
+        buttonText: {
+          type: String,
+          default: ''
+        },
+        buttonUrl: {
+          type: String,
+          default: 'asd'
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.hero {
+  width: 100%;
+  height: 40vh;
+  padding: 70px;
+  overflow: hidden;
+  background: no-repeat center center scroll;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+</style>
