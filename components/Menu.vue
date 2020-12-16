8d22fb0390380502ea08ff31a3c4408b2a232436
ahmet emre <ahmet@meditopia.com>
2020-12-16 11:54:22 +0300

    menu, footer and default layout done

---
+++ components/Menu.vue
@@ -0,0 +1,49 @@
+<template>
+  <nav>
+    <ul>
+      <li>
+        <nuxt-link exact to="/">
+          Welcome
+        </nuxt-link>
+      </li>
+      <li>
+        <nuxt-link exact to="/workflow">
+          Workflow
+        </nuxt-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {}
+</script>
+<style lang="scss" scoped>
+nav {
+  min-height: 50px;
+  padding: 5px 60px;
+  background: black;
+  color: white;
+  position: relative;
+  align-items: center;
+  display: flex;
+  justify-content: space-between;
+
+  a {
+    color: white;
+    text-decoration: none;
+    font-size: 25px;
+  }
+
+  ul {
+    display: flex;
+    align-items: center;
+    list-style: none;
+    padding: 0;
+
+    li {
+      padding-right: 40px;
+    }
+  }
+}
+</style>
