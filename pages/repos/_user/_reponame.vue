9bf9a297f6e69b865765e6a9c21d4f0cc3064678
ahmet emre <ahmet@meditopia.com>
2020-12-16 12:01:48 +0300

    repository detail page done with ui

--- pages/repos/_user/_reponame.vue
+++ pages/repos/_user/_reponame.vue
@@ -1,6 +1,19 @@
 <template>
   <main class="detail">
-    {{ repo }}
+    <div class="container">
+      <h1>{{ headline }}</h1>
+      <section>
+        <div class="row">
+          <div class="col-12 col-sm-12 col-md-6">
+            <p><span>Full Name:</span> {{ repo.full_name }}</p>
+            <p><span>Description:</span> {{ repo.description }}</p>
+            <p><span>Stars:</span> {{ repo.stargazers_count }}</p>
+            <p><span>Watchers:</span> {{ repo.watchers_count }}</p>
+            <a :href="repo.html_url" target="_blank">Go to Repository</a>
+          </div>
+        </div>
+      </section>
+    </div>
   </main>
 </template>
 
@@ -14,10 +27,23 @@ export default {
   },
   data () {
     return {
+      headline: 'Repository Detail'
     }
   }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+p {
+  margin-top: 20px;
+
+  span {
+    font-weight: 700;
+  }
+}
+
+a {
+  display: inline-block;
+  margin-top: 20px;
+}
 </style>
